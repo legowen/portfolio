83ed15e889b4be15a32669e7f89807699b8485f9
OwenSyKim <owen.sykim@gmail.com>
2024-03-01 00:13:14 -0800

    Update

--- src/assets/datas/skill.ts
+++ src/assets/datas/skill.ts
@@ -4,16 +4,13 @@ import css_icon from "../img/skill/css_icon.svg";
 import javascript_icon from "../img/skill/javascript_icon.svg";
 import typescript_icon from "../img/skill/typescript_icon.svg";
 import react_icon from "../img/skill/react_icon.svg";
-import redux_icon from "../img/skill/redux_icon.svg";
-import react_query_icon from "../img/skill/react_query_icon.svg";
-import axios_icon from "../img/skill/axios_icon.svg";
 import sass_scss_icon from "../img/skill/sass_scss_icon.svg";
-import styled_component_icon from "../img/skill/styled_component_icon.svg";
 import bootstrap_icon from "../img/skill/bootstrap_icon.svg";
+import mui_icon from "../img/skill/mui_icon.svg";
 import tailwind_icon from "../img/skill/tailwind_icon.svg";
-import git_icon from "../img/skill/git_icon.svg";
 import github_icon from "../img/skill/github_icon.svg";
 import node_icon from "../img/skill/node_js_icon.svg";
+import mongodb_icon from "../img/skill/mongodb_icon.svg";
 
 // Skill Components Data
 export let skill_List: { src: string; alt: string; name: string }[] = [
@@ -22,6 +19,7 @@ export let skill_List: { src: string; alt: string; name: string }[] = [
     alt: "HTML icon",
     name: "HTML",
   },
+
   {
     src: css_icon,
     alt: "CSS icon",
@@ -32,64 +30,58 @@ export let skill_List: { src: string; alt: string; name: string }[] = [
     alt: "JavaScript icon",
     name: "JavaScript",
   },
+  
   {
     src: typescript_icon,
     alt: "TypeScript icon",
     name: "TypeScript",
   },
+
   {
     src: react_icon,
     alt: "React icon",
     name: "React",
   },
-  {
-    src: redux_icon,
-    alt: "Redux icon",
-    name: "Redux",
-  },
-  {
-    src: react_query_icon,
-    alt: "React Query icon",
-    name: "React Query",
-  },
-  {
-    src: axios_icon,
-    alt: "Axios icon",
-    name: "Axios",
-  },
+
   {
     src: sass_scss_icon,
     alt: "Sass(SCSS) icon",
     name: "Sass(SCSS)",
   },
-  {
-    src: styled_component_icon,
-    alt: "Styled-Component icon",
-    name: "Styled-Component",
-  },
+
   {
     src: bootstrap_icon,
     alt: "Bootstrap icon",
     name: "Bootstrap",
   },
+
+  {
+    src: mui_icon,
+    alt: "MaterialUI icon",
+    name: "MaterialUI",
+  },
+
   {
     src: tailwind_icon,
     alt: "Tailwind icon",
     name: "Tailwind",
   },
-  {
-    src: git_icon,
-    alt: "Git icon",
-    name: "Git",
-  },
+
   {
     src: github_icon,
     alt: "GitHub icon",
     name: "GitHub",
   },
+
   {
     src: node_icon,
     alt: "Node icon",
     name: "Node",
+  },
+
+  {
+    src: mongodb_icon,
+    alt: "MongoDB icon",
+    name: "MongoDB",
   },
 ];
